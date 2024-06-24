c1b73f65f5f5d432707d658a1c9b1896afca6913
speedyfriend433 <speedyfriend13@gmail.com>
2024-06-24 14:52:50 +0900

    Update GradientPoint.swift

--- GradientPoint.swift
+++ GradientPoint.swift
@@ -4,12 +4,42 @@
 // Created by Speedyfriend67 on 24.06.24
 //
  
+//
+// GradientPoint.swift
+//
+// Created by Speedyfriend67 on 24.06.24
+//
+ 
 import SwiftUI
 
-struct GradientPoint: Identifiable, Equatable {
+class GradientPoint: Identifiable, ObservableObject, Equatable {
     let id = UUID()
-    var position: CGPoint
-    var color: Color
+    @Published var position: CGPoint
+    @Published var color: Color {
+        didSet {
+            if colorHistory.last != color {
+                colorHistory.append(color)
+            }
+        }
+    }
+    private var colorHistory: [Color] = []
+
+    init(position: CGPoint, color: Color) {
+        self.position = position
+        self.color = color
+        self.colorHistory = [color]
+    }
+
+    func undoColorChange() {
+        if colorHistory.count > 1 {
+            colorHistory.removeLast()
+            color = colorHistory.last!
+        }
+    }
+
+    static func == (lhs: GradientPoint, rhs: GradientPoint) -> Bool {
+        return lhs.id == rhs.id
+    }
 }
 
 extension Color {
